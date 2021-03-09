e5605e6f6c08943e2475f3b8eb41c90607f045ce
이미라 <emira1239@naver.com>
2021-03-09 12:54:06 +0900

    210309

---
+++ 2week/01 variable&print().py
@@ -0,0 +1,11 @@
+bollVar, intVar, floatVar, strVar = True, 0, 0.0, "hi"
+
+print("안녕하세요")
+
+print("100")
+print("%d" % 100)
+
+print("100 + 100")
+print("%d" % (100+100))
+
+print("%d %d" % (100, 200))
---
+++ 2week/02 input().py
@@ -0,0 +1,9 @@
+var1 = input()
+var2 = input()
+
+print(var1 + var2)
+
+var1 = int(input())
+var2 = int(input())
+
+print(var1 + var2)
