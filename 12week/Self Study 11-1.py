c59020f921d5c773f23b18fd4ba569d5de339b2d
이미라 <emira1239@naver.com>
2021-05-25 15:18:39 +0900

    12week-2

---
+++ 12week/Self Study 11-1.py
@@ -0,0 +1,12 @@
+def findMaxIdx(ary):
+    maxIdx = 0
+
+    for i in range(1, len(ary)):
+        if (ary[maxIdx] < ary[i]):
+            maxIdx = i
+    return maxIdx
+
+
+testAry = [55, 88, 33, 77]
+maxPos = findMaxIdx(testAry)
+print('최댓값 -->', testAry[maxPos])
